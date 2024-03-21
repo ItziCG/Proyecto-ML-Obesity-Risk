7c8fa3b2b5e19cac8a9d96a3eee7fb93ff4b2cd9
ItziCG <i.cid.garcia@gmail.com>
2024-03-21 19:40:19 +0100

    ghfgh

--- src/APP/app.py
+++ src/APP/app.py
@@ -1,40 +1,62 @@
-from flask import Flask, render_template, request
-import pickle
+from flask import Flask, request, render_template
+import joblib
+import pandas as pd
 
 app = Flask(__name__)
 
-# Cargar el modelo
-model = pickle.load(open('/Users/uxue/Desktop/Proyecto-ML-Obesity-Risk/src/model/model.pkl', 'rb'))
+# Cargar el modelo previamente entrenado
+modelo = joblib.load('modelo_obesidad.joblib')
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
+# Definir las columnas del DataFrame
+columnas = ['Genero', 'Edad', 'Altura', 'Peso', 'Antecedentes_obesidad', 'Consumo_calorias',
+            'Ingesta_vegetales', 'Num_comidas_principales', 'Consumo_entre_comidas', 'Fumador',
+            'Consumo_agua_diario', 'Monitoreo_calorias', 'Frecuencia_actividad_fisica',
+            'Tiempo_uso_tecnologico', 'Consumo_alcohol', 'Medio_transporte']
+
+@app.route('/')
+def index():
+    return render_template('formulario.html')
+
+@app.route('/prediccion', methods=['POST'])
+def predecir_obesidad():
     if request.method == 'POST':
-        # Obtener los valores ingresados por el usuario desde el formulario
-        age = float(request.form['age'])
-        height = float(request.form['height'])
-        weight = float(request.form['weight'])
-        fcvc = float(request.form['fcvc'])
-        ncp = float(request.form['ncp'])
-        caec = request.form['caec']
-        ch2o = float(request.form['ch2o'])
-        faf = float(request.form['faf'])
-        tue = float(request.form['tue'])
-        calc = request.form['calc']
-        mtrans = request.form['mtrans']
-        gender_male = 1 if request.form['gender'] == 'Male' else 0
-        family_yes = 1 if request.form['family'] == 'Yes' else 0
-        favc_yes = 1 if request.form['favc'] == 'Yes' else 0
-        smoke_yes = 1 if request.form['smoke'] == 'Yes' else 0
-        scc_yes = 1 if request.form['scc'] == 'Yes' else 0
-        
-        
-        # Realizar la predicción con el modelo
-        prediction = model.predict([[age, height, weight, fcvc, ncp, caec, ch2o, faf, tue, calc, mtrans, gender_male, family_yes, favc_yes, smoke_yes, scc_yes]])[0]
-        
-        # Devolver la predicción como respuesta
-        return render_template('result.html', prediction=prediction)
-    else:
-        return render_template('index.html')
+        # Obtener una respuesta del usuario
+        datos = {
+            'gender': request.form['genero'],
+            'age': int(request.form['edad']),
+            'height': float(request.form['altura']),
+            'weight': float(request.form['peso']),
+            'family': request.form['antecedentes_obesidad'],
+            'favc': request.form['consumo_calorias'],
+            'fcvc': int(request.form['ingesta_vegetales']),
+            'ncp': int(request.form['num_comidas_principales']),
+            'caec': request.form['consumo_entre_comidas'],
+            'smoke': request.form['fumador'],
+            'ch2o': int(request.form['consumo_agua_diario']),
+            'scc': request.form['monitoreo_calorias'],
+            'faf': int(request.form['frecuencia_actividad_fisica']),
+            'tue': int(request.form['tiempo_uso_tecnologico']),
+            'calc': request.form['consumo_alcohol'],
+            'mtrans': request.form['medio_transporte']
+        }
+
+        # Crear un DataFrame con la respuesta del usuario
+        df_usuario = pd.DataFrame([datos], columns=columnas)
+
+        # Realizar la predicción utilizando el modelo y el DataFrame del usuario
+        resultado = modelo.predict(df_usuario)
+
+        # Convertir el resultado a una etiqueta legible
+        if resultado == 0:
+            nivel_obesidad = 'Bajo'
+        elif resultado == 1:
+            nivel_obesidad = 'Medio'
+        else:
+            nivel_obesidad = 'Alto'
+
+        # Renderizar la plantilla de resultado y pasar el nivel de obesidad
+        return render_template('resultado.html', nivel_obesidad=nivel_obesidad)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
